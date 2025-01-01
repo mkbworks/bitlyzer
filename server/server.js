a6ef99b85c29f45fb3ab1eb94a8b0e77bbbcab69
Mahesh Kumaar Balaji <maheshkumaar.balaji@outlook.com>
2025-01-01 15:46:17 -0800

    Added graceful shutdown logic for express server instance

--- server/server.js
+++ server/server.js
@@ -1,12 +1,31 @@
 import express from "express";
+import figlet from "figlet";
+import chalk from "chalk";
 
-const server = express();
+const app = express();
 const ServerHost = process.env.HOST || "localhost";
 const ServerPort = process.env.PORT || 8080;
 
-server.use(express.json());
-server.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-server.listen(ServerPort, ServerHost, () => {
-    console.log(`Web server is running and listening at http://${ServerHost}:${ServerPort}`);
+const server = app.listen(ServerPort, ServerHost, () => {
+    let bannerText = chalk.magenta(figlet.textSync("BITLYZER", {
+        font: "Big",
+        horizontalLayout: "default",
+        verticalLayout: "default",
+        width: 100,
+    }));
+    let WebAddress = chalk.green(`http://${ServerHost}:${ServerPort}`)
+    console.log(bannerText);
+    console.log(`Application is running and listening for incoming requests at ${WebAddress}`);
+});
+
+process.on('SIGINT', () => {
+    console.log("Server Shutdown signal received.");
+    console.log("Initiating shutdown now.");
+    server.close(() => {
+        console.log("Server has been closed.");
+        process.exit(0);
+    });
 });
