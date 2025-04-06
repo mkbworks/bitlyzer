aec05bd92fb3d0f668c7cae8cad297845253aa1b
Mahesh Kumaar Balaji <maheshkumaar.balaji@outlook.com>
2025-04-06 16:21:28 -0700

    Updated the description for new URL registration

--- ui/src/components/Mylinks/Mylinks.jsx
+++ ui/src/components/Mylinks/Mylinks.jsx
@@ -6,7 +6,7 @@ function MyLinks() {
     return (
         <>
             <PageHeading Title="My Links" Icon={LinkIcon}>
-                Easily manage all your links in one place and customize properties like expiry date, target URL, action type, and more to suit your needs.
+                Manage all your active links in one place. Extend link expiry or modify link action quickly and easily.
             </PageHeading>
             <hr />
         </>
--- ui/src/components/ShortenUrl/ShortenUrl.jsx
+++ ui/src/components/ShortenUrl/ShortenUrl.jsx
@@ -7,28 +7,29 @@ function ShortenUrl() {
     return (
         <>
             <PageHeading Title="Shorten Url" Icon={CompressIcon}>
-                Generate a new shortened link that either redirects to or masks the original long URL, based on your preference.
+                Easily convert long, cluttered URLs into clean, shareable short links with our simple URL shortener tool. Whether you're managing links for social media, marketing campaigns, or personal use, this page lets you create a custom or auto-generated short URL in seconds. Just paste your long URL into the form, customize the alias if you'd like, and click generate. It's quick, reliable, and perfect for making your links easier to share, track, and manage.
             </PageHeading>
             <hr />
             <form className="form">
                 <div className="form-control">
                     <label id="TargetLabel">Paste your long  URL here!</label>
-                    <input type="text" name="Target" className="form-input" />
+                    <input type="text" name="Target" className="form-input" placeholder="Long URL to be masked or redirected to" />
                 </div>
                 <div className="form-control">
                     <label id="AliasLabel">(Optional) Define a custom short URL!</label>
-                    <input type="text" name="Alias" className="form-input" />
+                    <input type="text" name="Alias" className="form-input" placeholder="Enter a short URL of your own" />
                 </div>
                 <div className="form-control">
                     <label id="ActionLabel">What action is to be taken when URL is requested?</label>
                     <select className="form-select" name="Action">
+                        <option>Select one</option>
                         <option value="redirect">Redirect</option>
                         <option value="mask">Mask</option>
                     </select>
                 </div>
                 <div className="form-control">
                     <label id="ExpiryLabel">How long should the URL be valid?</label>
-                    <input type="number" name="Expiry" className="form-number" />
+                    <input type="number" name="Expiry" className="form-number" placeholder="Number of days till expiry" />
                 </div>
             </form>
         </>
